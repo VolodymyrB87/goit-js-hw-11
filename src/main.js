b99de9d0fca2d5ee12ec2319ebffb75cfe57ce12
VolodymyrB <volodymyrbedryk@gmail.com>
2026-03-25 21:19:17 +0100

    homework11

---
+++ src/main.js
@@ -0,0 +1,34 @@
+import { getImagesByQuery } from './js/pixabay-api';
+import {
+  clearGallery,
+  createGallery,
+  hideLoader,
+  showLoader,
+} from './js/rende-functions';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.js-form');
+form.addEventListener('submit', async e => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const qury = formData.get('search-text').trim();
+  clearGallery();
+  showLoader();
+  getImagesByQuery(qury)
+    .then(result => {
+      createGallery(result.hits);
+    })
+    .catch(erro => {
+      iziToast.error({
+        title: 'Error',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+      });
+    })
+    .finally(() => {
+      hideLoader();
+    });
+
+  form.reset();
+});
